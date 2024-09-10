3ef2dcaee54e784e1b054e13849bbcb2b51f22af
shahzeb8285 <shahzeb8285@gmail.com>
2024-09-10 18:55:40 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import { AxiosError } from "axios";
+import { Api, HttpClient } from "./src/dtpsApi";
+import { DTPSClient } from "./src/dtpsclient";
+require("dotenv").config()
+
+const main = async () => {
+ 
+    const url = "https://dtpspartnerapi-development.fly.dev/api/v1"
+    const apiKey =process.env.API_KEY?process.env.API_KEY:""
+    const apiSecret = process.env.API_SECRET? process.env.API_SECRET:""
+    const dtpsClient = new DTPSClient().init({
+        url,
+        apiKey,
+        apiSecret,
+    })
+  const resp = await    dtpsClient.card.listList()
+
+    console.log("resp", resp);
+};
+
+main();
---
+++ src/dtpsclient.ts
@@ -0,0 +1,76 @@
+import { Api, HttpClient } from "./dtpsApi";
+import crypto from "crypto";
+
+
+
+
+
+interface DTPSInitConfig {
+    url: string
+    apiKey: string,
+    apiSecret:string
+}
+
+
+const generateSignature = (secret: string, path: string, data: any) => {
+    const signature = crypto.createHmac("sha256", secret);
+    let message = path;
+    if (data) {
+      message += data;
+    }
+    signature.update(message);
+  
+    return signature.digest("hex");
+  };
+export class DTPSClient {
+
+
+    init(config:DTPSInitConfig) {
+       
+        
+        const httpClient = this.generateHTTPClient(config)
+        const api = new Api(httpClient);
+        return api
+    }
+
+
+    private generateHTTPClient({url,apiKey,apiSecret}:DTPSInitConfig ) {
+        const httpClient = new HttpClient({
+            baseURL:url,
+          });
+          httpClient.instance.interceptors.request.use(
+            (config) => {
+             
+              const finalData = config.data;
+              //@ts-ignore
+              const fullURL = config.baseURL + config.url;
+              const urlObj = new URL(fullURL);
+              const signature = generateSignature(apiSecret, urlObj.pathname, finalData);
+        
+              config.data = finalData;
+              config.headers["X-Api-Key"] = apiKey
+              config.headers["X-Api-Signature"] = signature;
+        
+              return config;
+            },
+            (error) => {
+              // Handle the error
+              return Promise.reject(error);
+            }
+          );
+        
+          httpClient.instance.interceptors.response.use(
+            (response) => {
+              return response.data;
+            },
+              (error) => {
+                  
+              return Promise.reject(error.response.data);
+            }
+          );
+        
+        
+        return httpClient
+    }
+
+}
